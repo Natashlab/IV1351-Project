2d9f7fa832157a580180f340a1c725d5d6be96ed
Natasha Donner <71009011+natashadonner@users.noreply.github.com>
2023-01-31 15:44:16 +0100

    Add files via upload

---
+++ Task4/src/main/java/model/Instruments.java
@@ -0,0 +1,26 @@
+package model;
+/*************************************************************************************************
+ * *  @author Natasha Donner
+ * *  Created: 02.01.2022
+ * *  Updated 08.01.2022
+ *************************************************************************************************/
+public class Instruments implements InstrumentsDTO{
+    int id;
+    String instrument;
+    String brand;
+    Boolean rented;
+    String price;
+
+    public Instruments(int id, String instrument, String brand, Boolean rented, String price) {
+        this.id = id;
+        this.instrument = instrument;
+        this.brand = brand;
+        this.rented = rented;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Instrument_id: " + id + ", Instrument: " + instrument + ", Brand: " + brand + ", Rented: " + rented + ", Price: " + price + "\n";
+    }
+}
